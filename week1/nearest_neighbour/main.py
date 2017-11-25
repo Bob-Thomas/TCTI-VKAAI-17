802052bef68239e05a273834772d31995560840d
Bob <smartcat007@hotmail.com>
2017-11-25 01:49:07 +0100

    Implemented knn simple style todo: comments

--- week1/nearest_neighbour/main.py
+++ week1/nearest_neighbour/main.py
@@ -1,35 +1,8 @@
 import numpy as np
 from math import sqrt
-import matplotlib.pyplot as plt
 import warnings
-from matplotlib import style
 from collections import Counter
-import pandas as pd
 import random
-
-style.use('fivethirtyeight')
-
-
-validation = np.genfromtxt('validation1.csv', delimiter=';', usecols=[
-                           0, 1, 2, 3, 4, 5, 6, 7])
-test_set = np.genfromtxt('dataset1.csv', delimiter=';',
-                         usecols=[0, 1, 2, 3, 4, 5, 6, 7])
-
-training = np.genfromtxt('days.csv', delimiter=';',
-                         usecols=[1, 2, 3, 4, 5, 6, 7])
-
-labels = []
-for label in test_set:
-    if label[0] < 20000301:
-        labels.append('winter')
-    elif 20000301 <= label[0] < 20000601:
-        labels.append('lente')
-    elif 20000601 <= label[0] < 20000901:
-        labels.append('zomer')
-    elif 20000901 <= label[0] < 20001201:
-        labels.append('herfst')
-    else:  # from 01−12 to end of year
-        labels.append('winter')
 
 
 def get_season_2000(date):
@@ -57,21 +30,6 @@ def get_season_2001(date):
     else:  # from 01−12 to end of year
         return 'winter'
 
-# [print (labels[x], test_set[x][1::]) for x in range(len(test_set))]
-# print(training)
-
-
-dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
-
-new_featues = [5, 7]
-
-# for i in dataset:
-#     for ii in dataset[i]:
-#         plt.scatter(ii[0], ii[1], s=100, color=i)
-
-# plt.scatter(new_featues[0], new_featues[1])
-# plt.show()
-
 
 def k_nearest_neighbours(data, predict, k=1):
     if len(data) <= k:
@@ -79,31 +37,38 @@ def k_nearest_neighbours(data, predict, k=1):
             "K is set to a value less than the total voting groups :'(")
 
     distances = []
+    predict = [predict[0], predict[1]]
     for group in range(len(data)):
-        for features in data[group][1::]:
-            distance = np.linalg.norm(np.array(features) - np.array(predict))
-            distances.append([distance, data[group][0]])
+        distances.append([
+            sqrt(sum([(a - b) ** 2 for a, b in zip(data[group][1::], predict)])),
+            data[group][0]]
+        )
     votes = [i[1] for i in sorted(distances)[:k]]
     vote_result = Counter(votes).most_common(1)[0][0]
-    return vote_result
+    return get_season_2000(vote_result)
 
 
-for k in range(1, 100):
-    new_days_2000 = [
-        get_season_2000(k_nearest_neighbours(test_set, trainer, k)) for trainer in training
+
+if __name__ == "__main__":
+    validation = np.genfromtxt('validation1.csv', delimiter=';', usecols=[0])
+    test_set = np.genfromtxt('dataset1.csv', delimiter=';',
+                            usecols=[0, 1, 2, 3, 4, 5, 6, 7])
+
+    training = np.genfromtxt('validation1.csv', delimiter=';',
+                            usecols=[1, 2, 3, 4, 5, 6, 7])
+
+
+    validation_labels = [
+        get_season_2001(trainer) for trainer in validation
     ]
 
-    new_days_2001 = [
-        get_season_2001(k_nearest_neighbours(test_set, trainer, k)) for trainer in training
-    ]
-
-    differences = [x for x in range(len(training)) if new_days_2000[x] != new_days_2001[x]]
-    # print(new_days_2000)
-    # print(new_days_2001)
-    print('%d%% : %d' % ((100 - (len(training) / 100 * len(differences) * 100)), k))
-# for i in dataset:
-#     for ii in dataset[i]:
-#         plt.scatter(ii[0], ii[1], s=100, color=i)
-
-# plt.scatter(new_featues[0], new_featues[1], color=result)
-# plt.show()
+    for k in range(1, 100):
+        new_days_2000 = [
+            k_nearest_neighbours(test_set, trainer, k) for trainer in training
+        ]
+        print(
+            '%d%% for k = %d' % (
+                (len(new_days_2000) - len([x for x in range(len(new_days_2000)) if new_days_2000[x] == validation_labels[x]])),
+                k
+            )
+        )
